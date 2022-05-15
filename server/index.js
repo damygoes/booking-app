dbd7ff82c314ab8230c31e46a43a3027bafde63a
Damilola Bada <damilolabada@Damilolas-MacBook-Pro.local>
2022-05-15 20:11:10 +0200

    booking app server created. Lessons: express server, nodemon, mongoDB creation, mongoose, yarn, nvm

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+// const express = require("express") // if you're using NodeJS, you need to use this line. However, for this project, I will use ES6 modules so I can use import and export commands. Therefore I need to add this line:  "type": "module", to the package.json
+import dotenv from "dotenv";
+// this line imports the .env file created to contain and hide the mongoDB data like username, password and DB name. Remember, to first run: "yarn add dotenv" before importing the file.
+import mongoose from "mongoose";
+
+// Importing all routes for the app
+import authRoute from "./routes/auth.js"; //if you're using import for files in express server, always add ".js" to the end of your imported files. For libraries, you don't need it.
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+const app = express();
+
+dotenv.config();
+// we need to configure the .env file before we can use it and that's what this line does. Now we can use our data and connect to mongoDB
+
+// API to mongoDB database
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO); //process.env.MONGO is the url that connects to the mongoDB from the .env file
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+// note that the function connect only tries to connect with mongoDB at the initialisation of the app, if it fails to connect to the database, it won't try to reconnect
+
+// However, the lines of code below: mongoose.connection.on will log the respective messages to the console once the first connection to the database is established and if anything happens to the database server, for example, a network problem on mongoDB, it will log to the console - "mongoDB disconnected" and the server will try to reconnect unlike the first connection established by the function  - "connect" defined above
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+// Creating Middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", authRoute);
+app.use("/api/hotels", authRoute);
+app.use("/api/rooms", authRoute);
+
+// To start the express server
+app.listen(8000, () => {
+  connect();
+  console.log("Connected to backend on Port: 8000!");
+});
+// note that in the package.json, I will add: "start": "nodemon index.js" to the scripts so I can start the server anytime and without restarting with: yarn start
